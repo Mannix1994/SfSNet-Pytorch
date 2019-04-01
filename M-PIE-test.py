79b189808725cdc05eab183f82f83f444159d67d
wang <wangmingzhang@outlook.com>
2019-04-01 15:44:09 +0800

    修复functions.py相关的bug

--- M-PIE-test.py
+++ M-PIE-test.py
@@ -7,28 +7,44 @@ import cv2
 import torch
 from config import M, LANDMARK_PATH, PROJECT_DIR
 from src.functions import create_shading_recon
-from src.mask import MaskGenerator
 from src.model import SfSNet
+
+M_PIE_DIR = '/home/creator/Projects/DL/MVCNN-keras-Face-Yale/data/M-PIE/train/001/*.png'
+
+
+class MaskGenerator(object):
+    def __init__(self):
+        super(MaskGenerator, self).__init__()
+
+    def align(self,  image, crop_size=(128, 128), scale=3.5):
+        image = cv2.copyMakeBorder(image, 70, 70, 70, 70, cv2.BORDER_CONSTANT)
+        image = cv2.resize(image, crop_size)
+        mask = np.ones(image.shape, dtype=np.uint8)*255
+        return mask, image
+
 
 if __name__ == '__main__':
     # define a SfSNet
-    net = SfSNet()
+    net = SfSNet().cuda()
     # set to eval mode
     net.eval()
     # load weights
     # net.load_weights_from_pkl('SfSNet-Caffe/weights.pkl')
     net.load_state_dict(torch.load('data/SfSNet.pth'))
     # define a mask generator
-    mg = MaskGenerator(LANDMARK_PATH)
+    mg = MaskGenerator()
 
     # get image list
-    image_list = glob.glob(os.path.join(PROJECT_DIR, 'Images/*.*'))
+    image_list = glob.glob(M_PIE_DIR)
 
     for image_name in image_list:
         # read image
         image = cv2.imread(image_name)
         # crop face and generate mask of face
         mask, im = mg.align(image, crop_size=(M, M))
+        cv2.imshow('mask', mask*255)
+        cv2.imshow('image', im)
+        # exit()
         # resize
         im = cv2.resize(im, (M, M))
         # normalize to (0, 1.0)
@@ -38,12 +54,12 @@ if __name__ == '__main__':
         im = np.expand_dims(im, 0)
 
         # get the normal, albedo and light parameter
-        normal, albedo, light = net(torch.from_numpy(im))
+        normal, albedo, light = net(torch.from_numpy(im).cuda())
 
         # get numpy array
-        n_out = normal.detach().numpy()
-        al_out = albedo.detach().numpy()
-        light_out = light.detach().numpy()
+        n_out = normal.cpu().detach().numpy()
+        al_out = albedo.cpu().detach().numpy()
+        light_out = light.cpu().detach().numpy()
 
         # -----------add by wang-------------
         # from [1, 3, 128, 128] to [128, 128, 3]
@@ -82,21 +98,19 @@ if __name__ == '__main__':
 
         # Note: n_out2, al_out2, light_out is the actual output
         Irec, Ishd = create_shading_recon(n_out2, al_out2, light_out)
-        # print(Irec.dtype, Ishd.dtype)
 
-        diff = (mask // 255)
+        diff = (mask // 255).astype(np.float32)
         n_out2 = n_out2 * diff
         al_out2 = al_out2 * diff
         Ishd = Ishd * diff
         Irec = Irec * diff
 
         # -----------add by wang------------
-        Ishd = cv2.cvtColor(Ishd, cv2.COLOR_RGB2GRAY)
-
         # al_out2 = (al_out2 / np.max(al_out2) * 255).astype(dtype=np.uint8)
         # Irec = (Irec / np.max(Irec) * 255).astype(dtype=np.uint8)
         # Ishd = (Ishd / np.max(Ishd) * 255).astype(dtype=np.uint8)
 
+        Ishd = cv2.cvtColor(Ishd, cv2.COLOR_RGB2GRAY)
         al_out2 = cv2.cvtColor(al_out2, cv2.COLOR_RGB2BGR)
         n_out2 = cv2.cvtColor(n_out2, cv2.COLOR_RGB2BGR)
         Irec = cv2.cvtColor(Irec, cv2.COLOR_RGB2BGR)
