dce702e46e8b320d11efbdd8d14b889a1a313a60
wang <wangmingzhang@outlook.com>
2019-04-20 11:10:24 +0800

    改进下预处理的流程

--- preprocess_dataset.py
+++ preprocess_dataset.py
@@ -3,10 +3,20 @@ from __future__ import absolute_import, division, print_function
 from config import *
 from src.dataset.real import preproccess_celaba_dataset
 from src.dataset.synthetic import preprocess_sfsnet_dataset
-
+import argparse
 
 if __name__ == '__main__':
-    preproccess_celaba_dataset(CELABA_DATASET_DIR, CELABA_DATASET_DIR_NPY,
-                               'data/temp_2019.04.19_19.00.10.pth', debug=True)
-    # preprocess_sfsnet_dataset(SFSNET_DATASET_DIR, SFSNET_DATASET_DIR_NPY, M)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', '--stage', default=0,
+                        help='define which dataset to preprocess. synthetic or real(celaba)')
+    parser.add_argument('-w', '--weights', help='define the path weights file from stage 0')
+    arg = parser.parse_args()
 
+    if arg.stage == '0':
+        print(arg)
+        preprocess_sfsnet_dataset(SFSNET_DATASET_DIR, SFSNET_DATASET_DIR_NPY, M)
+    elif arg.stage == '1':
+        print(arg)
+        preproccess_celaba_dataset(CELABA_DATASET_DIR, CELABA_DATASET_DIR_NPY, arg.weights, debug=True)
+    else:
+        raise RuntimeError("Wrong Option")
