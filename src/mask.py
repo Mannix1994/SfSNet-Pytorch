64f5121435fa86a699f822ebcd44d29d7d9acb0e
wang <wangmingzhang@outlook.com>
2019-04-23 12:29:19 +0800

    保存

--- src/mask.py
+++ src/mask.py
@@ -38,16 +38,21 @@ class MaskGenerator:
         face_rects = self._detector(gray_image, 0)
         return face_rects
 
-    def align(self, image, crop_size=(240, 240), scale=4, show_landmarks=False,
-              return_none=False, warp_and_crop=False):
+    def align(self, image, size=(240, 240), scale=4, warp=True, crop=True, resize=True,
+              return_none=False, show_landmarks=False):
         """
-        :param show_landmarks:
-        :param scale:
-        :param crop_size:
-        :type image: np.ndarray
-        :param image: BGR face image
-        :return: a face image with mask
+        warp and crop image
         https://blog.csdn.net/qq_39438636/article/details/79304130
+        :param image: BGR face image
+        :type image: np.ndarray
+        :param size:
+        :param scale:
+        :param warp:
+        :param crop:
+        :param resize:
+        :param return_none:
+        :param show_landmarks:
+        :return:
         """
         # get rectangles which contains face
         face_rects = self.bounding_boxes(image)
@@ -59,23 +64,146 @@ class MaskGenerator:
                 self.show_landmarks(image, landmarks)
             # create mask using landmarks
             mask = create_mask_fiducial(landmarks.T, image)
-            if warp_and_crop:
-                # warp and crop image
-                mask, image = self._warp_and_crop_face(image, mask, landmarks, crop_size, scale)
-            else:
-                return cv2.resize(mask, crop_size), cv2.resize(image, crop_size), True
+            if warp:
+                image, mask, r_mat = self._warp(image, mask, landmarks)
+                landmarks = self._get_rotated_landmarks(landmarks, r_mat)
+            if crop:
+                image = self._crop(image, landmarks, scale)
+                mask = self._crop(mask, landmarks, scale)
+
             # # detect rectangle
             # rect = self._detector(cv2.cvtColor(image, cv2.COLOR_BGR2GRAY), 0)
             # cv2.rectangle(mask, (rect[i].left(), rect[i].top()),
             #               (rect[i].right(), rect[i].bottom()), (255, 255, 0))
-            return mask, image, True
+            if resize:
+                return cv2.resize(mask, size), cv2.resize(image, size), True
+            else:
+                return mask, image, True
         else:
             sys.stderr.write("%s: Can't detect face in image\n" % __file__)
-            image = cv2.resize(image, crop_size)
+            image = cv2.resize(image, size)
             if return_none:
                 return None, image, False
             else:
-                return np.ones(image.shape, dtype=image.dtype)*255, image, False
+                return np.ones(image.shape, dtype=image.dtype) * 255, image, False
+
+    @staticmethod
+    def _get_rotated_landmarks(landmarks, rotate_mat):
+        # Blog； https://www.cnblogs.com/zhoug2020/p/7842808.html
+        # add 1 to every point
+        __padding = np.ones((landmarks.shape[0], 1), dtype=landmarks.dtype)
+        landmarks = np.concatenate([landmarks, __padding], axis=1)
+        # add [0, 0, 1] to rotate matrix
+        __padding = np.array([0, 0, 1], dtype=landmarks.dtype).reshape(1, 3)
+        rotate_mat = np.concatenate([rotate_mat, __padding], axis=0)
+        # compute rotated landmarks
+        rotate_landmarks = np.matmul(rotate_mat, landmarks.T)
+        # remove the padding and transpose landmarks
+        rotate_landmarks = rotate_landmarks[0:2, :].T
+        # return landmark as integer numpy array
+        return rotate_landmarks.astype(landmarks.dtype)
+
+    @staticmethod
+    def _warp(image, mask, landmarks):
+        """
+        warp image and mask by landmarks
+        :param image:
+        :type image np.ndarray
+        :param landmarks:
+        :type landmarks np.ndarray
+        :return: warped face and mask
+        """
+        # landmarks.shape = (68, 2)
+        landmarks = np.array(landmarks)
+        # compute rotate angle, r_angle=arctan((y1-y2)/(x1-x2))
+        # landmarks[36]: corner of left eye
+        # landmarks[42]: corner of right eye
+        r_angle = np.arctan((landmarks[36][1] - landmarks[42][1]) /
+                            (landmarks[36][0] - landmarks[42][0]))
+        r_angle = 180 * r_angle / np.pi
+        # get rotation matrix
+        rot_mat = cv2.getRotationMatrix2D(tuple(landmarks[2]), r_angle, scale=1)
+
+        # rotate image and mask
+        ims = image.shape[0:2]
+        rotated_image = cv2.warpAffine(image, rot_mat, dsize=(int(ims[0] * 1.3), int(ims[1] * 1.3)))
+        rotated_mask = cv2.warpAffine(mask, rot_mat, dsize=(int(ims[0] * 1.3), int(ims[1] * 1.3)))
+
+        return rotated_image, rotated_mask, rot_mat
+
+    @staticmethod
+    def _crop(image, landmarks, scale):
+        """
+        crop image by face landmarks
+        :param image:
+        :param landmarks:
+        :param scale:
+        :return:
+        """
+        # left eye: landmarks[36]
+        # left mouth: landmarks[48]
+        # nose: landmarks[29]
+        # find the most left point and most right point
+        landmarks_x = landmarks[:, 0]
+        most_left_x = np.min(landmarks_x)
+        most_right_x = np.max(landmarks_x)
+        mid_x = (most_left_x + most_right_x) // 2
+        # print(most_left_x, most_right_x, mid_x)
+        # define new center point use mid_x and y from nose point
+        center_point = [mid_x, landmarks[29][1]]
+        # compute the distance between left eye(landmarks[36])
+        distance = most_right_x - mid_x
+        size = distance * scale
+        # print(center_point)
+        # compute row_start, row_end, col_start, col_end
+        row_start = int(center_point[1] - size / 2.0)
+        row_end = int(center_point[1] + size / 2.0)
+        col_start = int(center_point[0] - size / 2.0)
+        col_end = int(center_point[0] + size / 2.0)
+        # print('*' * 10)
+        # print(row_start, row_end, col_start, col_end)
+        # make range valid and compute padding
+        if row_start < 0:
+            padding_up = abs(row_start)
+            row_start = 0
+        else:
+            padding_up = 0
+        if col_start < 0:
+            padding_left = abs(col_start)
+            col_start = 0
+        else:
+            padding_left = 0
+        if row_end > (image.shape[0] - 1):
+            padding_down = row_end - (image.shape[0] - 1)
+            row_end = image.shape[0] - 1
+        else:
+            padding_down = 0
+        if col_end > (image.shape[1] - 1):
+            padding_right = col_end - (image.shape[1] - 1)
+            col_end = image.shape[1] - 1
+        else:
+            padding_right = 0
+        # print(row_start, row_end, col_start, col_end)
+        # print('*' * 10)
+        # crop image
+        cropped_image = image[row_start:row_end, col_start:col_end]
+
+        # add padding to image
+        rows, cols, _ = cropped_image.shape
+        if padding_up > 0:
+            padding = np.zeros(shape=(padding_up, cols, 3), dtype=cropped_image.dtype)
+            cropped_image = np.vstack((padding, cropped_image))
+        if padding_down > 0:
+            padding = np.zeros(shape=(padding_down, cols, 3), dtype=cropped_image.dtype)
+            cropped_image = np.vstack((cropped_image, padding))
+        rows, cols, _ = cropped_image.shape
+        if padding_left > 0:
+            padding = np.zeros(shape=(rows, padding_left, 3), dtype=cropped_image.dtype)
+            cropped_image = np.hstack((padding, cropped_image))
+        if padding_right > 0:
+            padding = np.zeros(shape=(rows, padding_right, 3), dtype=cropped_image.dtype)
+            cropped_image = np.hstack((cropped_image, padding))
+        return cropped_image
 
     def _warp_and_crop_face(self, image, mask, landmarks, crop_size, scale):
         """
@@ -92,16 +220,16 @@ class MaskGenerator:
         # compute rotate angle, r_angle=arctan((y1-y2)/(x1-x2))
         # landmarks[36]: corner of left eye
         # landmarks[42]: corner of right eye
-        r_angle = np.arctan((landmarks[36][1]-landmarks[42][1]) /
-                            (landmarks[36][0]-landmarks[42][0]))
-        r_angle = 180*r_angle/np.pi
+        r_angle = np.arctan((landmarks[36][1] - landmarks[42][1]) /
+                            (landmarks[36][0] - landmarks[42][0]))
+        r_angle = 180 * r_angle / np.pi
         # get rotation matrix
         rot_mat = cv2.getRotationMatrix2D(tuple(landmarks[2]), r_angle, scale=1)
 
         # rotate image and mask
         ims = image.shape[0:2]
-        rotated_image = cv2.warpAffine(image, rot_mat, dsize=(ims[0]*2, ims[1]*2))
-        rotated_mask = cv2.warpAffine(mask, rot_mat, dsize=(ims[0]*2, ims[1]*2))
+        rotated_image = cv2.warpAffine(image, rot_mat, dsize=(ims[0] * 2, ims[1] * 2))
+        rotated_mask = cv2.warpAffine(mask, rot_mat, dsize=(ims[0] * 2, ims[1] * 2))
 
         # crop image and mask
         cropped_image = self._crop_image(rotated_image, landmarks, scale)
@@ -116,14 +244,14 @@ class MaskGenerator:
     def _crop_image(self, image, landmarks, scale):
         # compute the distance between left eye(landmarks[36])
         # and left mouth point[landmarks[48]]
-        distance = np.sqrt(np.sum(np.square(landmarks[36]-landmarks[48])))
+        distance = np.sqrt(np.sum(np.square(landmarks[36] - landmarks[48])))
         size = distance * scale
         # compute row_start, row_end, col_start, col_end
         landmark_nose = landmarks[30]
-        row_start = int(landmark_nose[1]-size/2.0)
-        row_end = int(landmark_nose[1]+size/2.0)
-        col_start = int(landmark_nose[0]-size/2.0)
-        col_end = int(landmark_nose[0]+size/2.0)
+        row_start = int(landmark_nose[1] - size / 2.0)
+        row_end = int(landmark_nose[1] + size / 2.0)
+        col_start = int(landmark_nose[0] - size / 2.0)
+        col_end = int(landmark_nose[0] + size / 2.0)
         # print('*' * 10)
         # print(row_start, row_end, col_start, col_end)
         # make range valid
@@ -143,7 +271,7 @@ class MaskGenerator:
         _min = np.min((rows, cols))
         if _min < cols:
             # rows is smaller than cols
-            padding = np.zeros(shape=(cols-_min, cols, 3), dtype=cropped_image.dtype)
+            padding = np.zeros(shape=(cols - _min, cols, 3), dtype=cropped_image.dtype)
             cropped_image = np.vstack((cropped_image, padding))
         elif _min < rows:
             # cols is smaller than rows
@@ -151,7 +279,8 @@ class MaskGenerator:
             cropped_image = np.hstack((cropped_image, padding))
         return cropped_image
 
-    def show_landmarks(self, image, landmarks):
+    @staticmethod
+    def show_landmarks(image, landmarks):
         im = image.copy()
         for i, landmark in enumerate(landmarks):
             cv2.circle(im, tuple(landmark), 3, (0, 0, 255))
@@ -162,9 +291,16 @@ class MaskGenerator:
 
 
 if __name__ == '__main__':
-    from config import LANDMARK_PATH
-    image = cv2.imread('Images/4.png_face.png')
+    from config import LANDMARK_PATH, PROJECT_DIR
+    import os
+    import glob
+
+    images = glob.glob(os.path.join(PROJECT_DIR, '周/*.png'))
     mask_gen = MaskGenerator(LANDMARK_PATH)
-    mah= mask_gen.get_masked_face(image)
-    cv2.imshow('123', mah)
-    cv2.waitKey(0)
+    for _path in images:
+        _im = cv2.imread(_path)
+        ma, im, _ = mask_gen.align(_im, warp=True, crop=False, resize=False)
+        cv2.imshow('mask', ma)
+        cv2.imshow('image', im)
+        if cv2.waitKey(0) == 27:
+            exit()
