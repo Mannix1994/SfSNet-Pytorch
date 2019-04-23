52f126e5b03ca623da658f501d34689cdb9c801f
wang <wangmingzhang@outlook.com>
2019-04-23 19:53:27 +0800

    改进流程

--- preprocess_dataset.py
+++ preprocess_dataset.py
@@ -10,13 +10,13 @@ if __name__ == '__main__':
     parser.add_argument('stage', default=0, type=int,
                         help='define which dataset to preprocess. synthetic or real(CElABA)')
     parser.add_argument('-w', '--weights', help='define the path weights file from stage 0')
+    parser.add_argument('-s', '--show', help='show process result', action='store_true')
     arg = parser.parse_args()
+    print(arg)
 
     if arg.stage == 0:
-        print(arg)
         preprocess_sfsnet_dataset(SFSNET_DATASET_DIR, SFSNET_DATASET_DIR_NPY, M)
     elif arg.stage == 1 and arg.weights:
-        print(arg)
-        preproccess_celaba_dataset(CELABA_DATASET_DIR, CELABA_DATASET_DIR_NPY, arg.weights, debug=True)
+        preproccess_celaba_dataset(CELABA_DATASET_DIR, CELABA_DATASET_DIR_NPY, arg.weights, debug=arg.show)
     else:
         raise RuntimeError("Wrong Option")
